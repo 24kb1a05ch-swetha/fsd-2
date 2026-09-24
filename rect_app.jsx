db0cc8363ca711843fcd0e07c8a46e53963ac3f1
24kb1a05ch-swetha <24kb1a05ch@nbkrist.org>
2026-09-24 09:25:16 +0530

    Add files via upload

---
+++ rect_app.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+  const message = "Hello, React!";
+
+  return (
+    <div>
+      <h1>{message}</h1>
+      <p>{"This text is also a string literal."}</p>
+    </div>
+  );
+}
+
+export default App;
